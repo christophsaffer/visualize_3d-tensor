bfcc4512153207715570b46a7a24050b9b8cb01d
Christoph Saffer <Christoph.Saffer@uni-jena.de>
2019-09-02 12:00:02 +0200

    Changed locale to en_en for read in floats correctly

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <QApplication>
 
+#include <locale.h>
 #include <viewer.hpp>
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
   QApplication application(argc, argv);
 
   QSurfaceFormat fmt;
   fmt.setSamples(10);
   QSurfaceFormat::setDefaultFormat(fmt);
+  setlocale(LC_ALL, "C");
 
   viewer v{};
   return application.exec();
